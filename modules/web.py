f5cdff2aa88a800d5d51aefd08cdeb7c9bbf59f6
PyCraftsman <vaibhavvj2001@gmail.com>
2023-11-25 21:42:56 +0530

    manage_sources file created

--- modules/web.py
+++ modules/web.py
@@ -6,3 +6,12 @@ def index():
         return render_template('index.html', var = var1)
     except Exception as e:
         print("exception while rendering index page : "+ str(e))
+
+@app.route('/home')
+def home():
+    try:
+        var1 = "Welcome to the Python Flask"
+        return render_template('manage_resources.html', var = var1)
+    except Exception as e:
+        print("exception while rendering index page : "+ str(e))
+                                
