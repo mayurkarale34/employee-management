6c8a20be7e596181b889a85475effb766febf96a
PyCraftsman <vaibhavvj2001@gmail.com>
2023-12-04 18:34:20 +0530

    task 14 15 16

--- modules/web.py
+++ modules/web.py
@@ -14,4 +14,20 @@ def home():
         return render_template('manage_resources.html', var = var1)
     except Exception as e:
         print("exception while rendering index page : "+ str(e))
+                
+@app.route('/helpd')
+def helpd():
+    try:
+        var1 = "Welcome to the Python Flask"
+        return render_template('helpdesk.html', var = var1)
+    except Exception as e:
+        print("exception while rendering index page : "+ str(e))
+
+@app.route('/analitics')
+def analitics():
+    try:
+        var1 = "Welcome to the Python Flask"
+        return render_template('analitics.html', var = var1)
+    except Exception as e:
+        print("exception while rendering index page : "+ str(e))        
                                 
