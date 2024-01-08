bb4b4b1ed3e96ff5da96ea92b906be8038230920
mayurkarale34 <mayurkarale34@gmail.com>
2024-01-08 21:39:22 +0530

    attendance fixes

--- modules/manage_attendance/api/manage_attendance.py
+++ modules/manage_attendance/api/manage_attendance.py
@@ -30,3 +30,33 @@ def api_add_attendance():
         transaction.rollback()
         connection.close()
         return jsonify(response)          
+
+@app.route('/api/v1/get-daily-attendance', methods=['POST'])
+@runtime_logger
+def api_get_daily_attendance():
+    response = {"status" : False, "message" : ""}
+    try:
+        data = dict(request.get_json())
+        
+        attendance_date = datetime.now().strftime('%Y-%m-%d')
+
+        existing_data = app._engine.connect().execute(text(f"SELECT id, date_format(clock_in, '%H:%i:%S') as clock_in, date_format(clock_out, '%H:%i:%S') as clock_out, date_format(working_hours, '%H:%i:%S') as working_hours, date_format(attendance_date,'%d-%m-%Y') as attendance_date FROM tb_attendance WHERE employee_id = '{data['employee_id']}' AND attendance_date = '{attendance_date}'"))
+
+        if existing_data.rowcount:
+            row = existing_data.fetchone()
+            columns = existing_data.keys()
+            row_dict = dict(zip(columns, row))
+
+        else:
+            row_dict = {
+                "id" : "",
+                "clock_in" : "",
+                "clock_out" : "",
+                "working_hours" : "",
+                "attendance_date" : attendance_date
+            }
+        return jsonify(row_dict)
+    except Exception as e:
+        print("Error while Clock In, Please contact administrator. : "+ str(e))
+        return jsonify({})      
+    
